f98a659af7bb2ecdc99dfb726f87dcc735f2a50b
henry <atime4ux@nate.com>
2022-05-26 20:25:06 +0900

    zip after download

--- LibWebToonCrawler/CrawlerEngine.cs
+++ LibWebToonCrawler/CrawlerEngine.cs
@@ -1,4 +1,5 @@
-﻿using LibWebToonCrawler.Model;
+﻿using System.IO.Compression;
+using LibWebToonCrawler.Model;
 using LibWebToonCrawler.Parser;
 using Newtonsoft.Json;
 using System;
@@ -44,62 +45,92 @@ namespace LibWebToonCrawler
                     {
                         LogAction.WriteStatus($"downloading [{info.SiteName}] data");
 
-                        List<CrawlingItem> lstAll = await parsing.GetParsingList();
+                        List<CrawlingItem> lstItem = await parsing.GetParsingList();
 
-                        LogAction.WriteStatus($"{parsing.GetParsingTarget()} : success get {lstAll.Count} items");
-                        LogAction.WriteItem(lstAll);
+                        LogAction.WriteStatus($"{parsing.GetParsingTarget()} : success get {lstItem.Count} items");
+                        LogAction.WriteItem(lstItem);
 
                         //다운로드 처리
-                        using (var webClient = new System.Net.WebClient())
-                        {
-                            webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.33 Safari/537.36");
-
-                            string baseDir = System.IO.Directory.GetCurrentDirectory();
-                            int idx = 0;
-                            int prevNumber = 0;
-                            foreach (var item in lstAll)
-                            {
-                                if (prevNumber != item.ItemNumber)
-                                {
-                                    if (prevNumber > 0)
-                                    {
-                                        var prevItem = lstAll.First(x => x.ItemNumber == prevNumber);
-                                        LogAction.WriteStatus($"download img end : {prevItem.ItemTitle} - {prevItem.ItemNumber}");
-                                    }
-
-                                    //초기화
-                                    idx = 0;
-                                    prevNumber = item.ItemNumber;
-
-                                    LogAction.WriteStatus($"download img start : {item.ItemTitle} - {item.ItemNumber}");
-                                }
-                                idx++;
-
-                                string itemDir = $"{baseDir}\\{item.ItemTitle}-{item.ItemNumber}";
-                                if (System.IO.Directory.Exists(itemDir) == false)
-                                {
-                                    System.IO.Directory.CreateDirectory(itemDir);
-                                }
-
-                                string imgExt = System.IO.Path.GetExtension(item.ItemUrl).Replace(".", "");
-                                string imgFileName = $"{idx.ToString().PadLeft(5, '0')}.{imgExt}";
-
-                                try
-                                {
-                                    webClient.DownloadFile(item.ItemUrl, $"{itemDir}\\{imgFileName}");
-                                }
-                                catch (Exception ex)
-                                {
-                                    LogAction.WriteStatus($"download img error : {item.ItemTitle} - {item.ItemNumber} - {idx} - {item.ItemUrl}\r\n{ex.Message}");
-                                }
-                            }
-                            
-                        }
+                        Download(lstItem);
                     }
                 }
             }
 
             LogAction.WriteStatus("finish\r\n====================");
+        }
+
+        private void Download(List<CrawlingItem> lstItem)
+        {
+            string baseDir = $"{System.IO.Directory.GetCurrentDirectory()}\\download";
+
+            Func<CrawlingItem, string> getDownlaodPath = (ci) => {
+                string path = $"{baseDir}\\{ci.ItemTitle}_{ci.ItemNumber}";
+                if (System.IO.Directory.Exists(path) == false)
+                {
+                    System.IO.Directory.CreateDirectory(path);
+                }
+
+                return path;
+            };
+
+            Func<CrawlingItem, int, string> getImageFilePath = (ci, i) => {
+                string path = getDownlaodPath(ci);
+                string ext = System.IO.Path.GetExtension(ci.ItemUrl).Replace(".", "");
+                string fileName = $"{(i + 1).ToString().PadLeft(5, '0')}.{ext}";
+
+                return $"{path}\\{fileName}";
+            };
+
+            Action<CrawlingItem> zipImg = (pi) => {
+                //압축
+                string prevPath = getDownlaodPath(pi);
+                ZipFile.CreateFromDirectory(prevPath, $"{baseDir}\\{pi.ItemTitle}_{pi.ItemNumber}.zip");
+
+                //압축 후 삭제
+                System.IO.Directory.Delete(prevPath, true);
+            };
+
+            using (var webClient = new System.Net.WebClient())
+            {
+                webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.33 Safari/537.36");
+
+                int itemIdx = 0;
+                int fileIdx = 0;
+                foreach (var item in lstItem)
+                {
+                    if (fileIdx == 0)
+                    {
+                        LogAction.WriteStatus($"download img start : {item.ItemTitle} - {item.ItemNumber}");
+                    }
+
+                    try
+                    {
+                        webClient.DownloadFile(item.ItemUrl, getImageFilePath(item, fileIdx));
+                    }
+                    catch (Exception ex)
+                    {
+                        LogAction.WriteStatus($"download img error : {item.ItemTitle} - {item.ItemNumber} - {fileIdx + 1} - {item.ItemUrl}\r\n{ex.Message}");
+                    }
+
+                    if ((itemIdx + 1) == lstItem.Count
+                        || lstItem[itemIdx + 1].ItemTitle != item.ItemTitle
+                        || lstItem[itemIdx + 1].ItemNumber != item.ItemNumber)
+                    {
+                        //마지막 or 다음 항목과 다른 경우 압축 후 삭제
+                        zipImg(item);
+                        LogAction.WriteStatus($"download img end : {item.ItemTitle} - {item.ItemNumber}");
+
+                        //마커 초기화
+                        fileIdx = 0;
+                    }
+                    else
+                    {
+                        fileIdx++;
+                    }
+
+                    itemIdx++;
+                }
+            }
         }
 
 
