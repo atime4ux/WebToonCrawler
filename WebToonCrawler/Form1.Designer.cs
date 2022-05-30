eeb6414b6e0b3495c0107908ddcbd2e48630994a
atime4ux <atime4ux@nate.com>
2022-05-31 08:05:25 +0900

    fix element anchor

--- WebToonCrawler/Form1.Designer.cs
+++ WebToonCrawler/Form1.Designer.cs
@@ -39,9 +39,10 @@
             // btnRun
             // 
             this.btnRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRun.Location = new System.Drawing.Point(566, 12);
+            this.btnRun.Location = new System.Drawing.Point(691, 15);
+            this.btnRun.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnRun.Name = "btnRun";
-            this.btnRun.Size = new System.Drawing.Size(90, 23);
+            this.btnRun.Size = new System.Drawing.Size(103, 29);
             this.btnRun.TabIndex = 8;
             this.btnRun.Text = "실행";
             this.btnRun.UseVisualStyleBackColor = true;
@@ -50,33 +51,37 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 17);
+            this.label2.Location = new System.Drawing.Point(11, 21);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.Size = new System.Drawing.Size(100, 15);
             this.label2.TabIndex = 6;
             this.label2.Text = "검색정보 json";
             // 
             // txtLog
             // 
-            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtLog.Location = new System.Drawing.Point(470, 41);
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(536, 52);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtLog.Multiline = true;
             this.txtLog.Name = "txtLog";
             this.txtLog.ReadOnly = true;
             this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtLog.Size = new System.Drawing.Size(302, 237);
+            this.txtLog.Size = new System.Drawing.Size(390, 295);
             this.txtLog.TabIndex = 13;
             // 
             // txtItemList
             // 
-            this.txtItemList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtItemList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtItemList.Location = new System.Drawing.Point(12, 284);
+            this.txtItemList.Location = new System.Drawing.Point(14, 355);
+            this.txtItemList.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtItemList.Multiline = true;
             this.txtItemList.Name = "txtItemList";
             this.txtItemList.ReadOnly = true;
             this.txtItemList.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtItemList.Size = new System.Drawing.Size(760, 157);
+            this.txtItemList.Size = new System.Drawing.Size(912, 195);
             this.txtItemList.TabIndex = 16;
             this.txtItemList.WordWrap = false;
             // 
@@ -84,34 +89,34 @@
             // 
             this.lblSleepRemain.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lblSleepRemain.AutoSize = true;
-            this.lblSleepRemain.Location = new System.Drawing.Point(662, 17);
+            this.lblSleepRemain.Location = new System.Drawing.Point(801, 21);
             this.lblSleepRemain.Name = "lblSleepRemain";
-            this.lblSleepRemain.Size = new System.Drawing.Size(106, 13);
+            this.lblSleepRemain.Size = new System.Drawing.Size(122, 15);
             this.lblSleepRemain.TabIndex = 18;
             this.lblSleepRemain.Text = "잠자기 남은 시간";
             // 
             // txtCrawlingInfoJson
             // 
-            this.txtCrawlingInfoJson.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtCrawlingInfoJson.Location = new System.Drawing.Point(12, 41);
+            this.txtCrawlingInfoJson.Location = new System.Drawing.Point(14, 51);
+            this.txtCrawlingInfoJson.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtCrawlingInfoJson.Name = "txtCrawlingInfoJson";
-            this.txtCrawlingInfoJson.Size = new System.Drawing.Size(452, 237);
+            this.txtCrawlingInfoJson.Size = new System.Drawing.Size(516, 295);
             this.txtCrawlingInfoJson.TabIndex = 20;
             this.txtCrawlingInfoJson.Text = "";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 453);
+            this.ClientSize = new System.Drawing.Size(940, 566);
             this.Controls.Add(this.txtCrawlingInfoJson);
             this.Controls.Add(this.lblSleepRemain);
             this.Controls.Add(this.txtItemList);
             this.Controls.Add(this.txtLog);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnRun);
-            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(569, 613);
             this.Name = "Form1";
             this.Text = "WebToonCrawler";
             this.Load += new System.EventHandler(this.Form1_Load);
